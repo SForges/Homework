fefa31a1bb130724d3c0c505efc988afc513df64
SForges <101294609+SForges@users.noreply.github.com>
2022-04-09 15:32:55 +0300

    Update Homework5.cpp

--- Homework5.cpp
+++ Homework5.cpp
@@ -22,6 +22,9 @@ public:
     Complex Mult(Complex A) {
         return Complex(real * A.real - image * A.image, real * A.image + A.real * image);
     }
+    Complex Div(Complex A) {
+        return Complex((real * A.real + image * A.image) / (A.real * A.real + A.image * A.image), (A.real * image - real * A.image) / (A.real * A.real + A.image * A.image));
+    }
     void Print() {
         cout << real << "+" << image << "i" << endl;
     }
@@ -29,11 +32,11 @@ public:
 
 int main()
 {
-    Complex* Alpha = new Complex(5.0, 11.0);
-    Complex* Beta = new Complex(1.0, 2.0);
+    Complex* Alpha = new Complex(5, 2);
+    Complex* Beta = new Complex(3, 4);
     Alpha->Sum(*Beta).Print();
     Alpha->Diffr(*Beta).Print();
     Alpha->Mult(*Beta).Print();
-    
+    Alpha->Div(*Beta).Print();
 
 }
